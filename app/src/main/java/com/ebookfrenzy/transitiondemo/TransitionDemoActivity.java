e28482275d3d425de2a14c08cd414121ce4e2063
Jeremy (JJ) Bernsdorff <jpachoulli@gmail.com>
2018-06-23 14:51:24 -0700

    Android 3.0 Development Essentials Android 8 Edition Chapter 33 - And Android Transition Tutorial using 'beginDelayedTransition'

---
+++ app/src/main/java/com/ebookfrenzy/transitiondemo/TransitionDemoActivity.java
@@ -0,0 +1,55 @@
+package com.ebookfrenzy.transitiondemo;
+
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.transition.ChangeBounds;
+import android.transition.Transition;
+import android.transition.TransitionManager;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.BounceInterpolator;
+import android.widget.Button;
+
+public class TransitionDemoActivity extends AppCompatActivity {
+
+    ConstraintLayout myLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transition_demo);
+
+        myLayout = findViewById(R.id.myLayout);
+
+        myLayout.setOnTouchListener(
+                new ConstraintLayout.OnTouchListener() {
+                    public boolean onTouch(View v, MotionEvent m) {
+                        handleTouch();
+                        return true;
+                    }
+                }
+        );
+    }
+
+    public void handleTouch() {
+        Button button = findViewById(R.id.myButton);
+
+        Transition changeBounds = new ChangeBounds();
+        changeBounds.setDuration(3000);
+        changeBounds.setInterpolator(new BounceInterpolator());
+
+        TransitionManager.beginDelayedTransition(myLayout, changeBounds);
+
+        button.setMinimumWidth(500);
+        button.setMinimumHeight(350);
+
+        ConstraintSet set = new ConstraintSet();
+
+        set.connect(R.id.myButton, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM, 0);
+        set.connect(R.id.myButton, ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT, 0);
+        set.constrainWidth(R.id.myButton, ConstraintSet.WRAP_CONTENT);
+        set.applyTo(myLayout);
+    }
+}
